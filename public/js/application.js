4bab707ac0b5205e0db9f70aeeebd7e6e65b29c0
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-07 11:52:00 -0600

    .

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 
+  $('#get_color').click(function(event) {
+    event.preventDefault();
+    $.post("/color", function(color) {
+      var cell_color = color;
+      var cell_pos = Math.floor(Math.random() * 9) + 1;
+      
+       $('#color_me').find($("li:nth-child(" + cell_pos + ")")).css('backgroundColor', cell_color)
+      // console.log(finder)
+      // finder.style.backgroundColor="#FFF";
+    })
+
+  })
+
 });
